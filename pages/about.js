6a4e975028dab11e0676acd065ff81e80c1f56b3
kangkyeongseo <kks_big@naver.com>
2022-07-29 18:03:05 +0900

    Static and Pre Rendering

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function Name() {
-  return <h1>about</h1>;
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  return (
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
+  );
 }
-0;
