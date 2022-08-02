8d31b25705dc8e6123baef2faf96912921d4ce77
kangkyeongseo <kks_big@naver.com>
2022-08-02 15:51:16 +0900

    Layout Pattern

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,21 +5,38 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/">
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 20px 0px 0px 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
       `}</style>
     </nav>
